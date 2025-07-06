74fc26e95963250c69f28cb65d068a795a261a25
ChaserFrank <wangangafrancis063@gmail.com>
2025-07-06 16:43:06 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   build: {
-    outDir: 'dist',
+    outDir: '../server/dist',
     assetsDir: 'assets',
     sourcemap: false,
     minify: 'terser',
