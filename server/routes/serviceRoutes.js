fd82030f343297e86edd5e40a4f9278a7058d41b
Chaserfrank <wangangafrancis063@gmail.com>
2025-07-14 11:40:06 +0300

    Added Service routes

---
+++ server/routes/serviceRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {
+  getAllServices,
+  getServiceById,
+  createService,
+  updateService,
+  deleteService
+} from '../controllers/serviceController.js';
+import { authenticateToken, requireAdmin } from '../middleware/auth.js';
+
+const router = express.Router();
+
+// Public routes
+router.get('/', getAllServices);
+router.get('/:id', getServiceById);
+
+// Admin routes (protected)
+router.post('/', authenticateToken, requireAdmin, createService);
+router.put('/:id', authenticateToken, requireAdmin, updateService);
+router.delete('/:id', authenticateToken, requireAdmin, deleteService);
+
+export default router;
